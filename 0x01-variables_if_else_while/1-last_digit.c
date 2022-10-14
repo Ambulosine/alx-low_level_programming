1e7c5de68609114fc18183a1435a4b2ef444181a
Ambulose <ambulose2019@gmail.com>
2022-10-14 15:04:52 +0100

    last fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,14 +15,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	printf("Last digit of %i is %i and is ", n, (n % 10));
-
-	if ((n % 10) < 6)
-	{
-	printf("less than 6 and not 0\n");
-	}
-	else if ((n % 10) == 0)
+	if ((n % 10) == 0)
 	{
 	printf("0\n");
+	}
+	else if ((n % 10) < 6)
+	{
+	printf("less than 6 and not 0\n");
 	}
 	else
 	{
