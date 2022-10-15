ca45fbeff52485a8cf39088b29b01b7feff4aba0
Ambulose <ambulose2019@gmail.com>
2022-10-15 11:22:29 +0100

    fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,39 +1,27 @@
 #include <stdio.h>
 
 /**
- *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
+ *main - print 00 to 99 with no duplicate digits or combos: no 11, no 10 (01)
+ *
  *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int tens;
 	int ones;
-	int t;
-	int o;
+	int tens;
 
 	for (tens = '0'; tens <= '9'; tens++)
 	{
-		for (ones = '0'; ones <= '9'; ones++)
+		for (ones = (tens + 1); ones <= '9'; ones++)
 		{
-			for (t = tens; t <= '9'; t++)
-			{
-				for (o = ones + 1; o <= '9'; o++)
-				{
-					putchar(tens);
-					putchar(ones);
-					putchar(' ');
-					putchar(t);
-					putchar(o);
+			putchar(tens);
+			putchar(ones);
 
-					if (!((tens == '9' && ones == '8') &&
-					      (t == '9' && o == '9')))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				o = '0';
+			if (tens != '8' || ones != '9')
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
