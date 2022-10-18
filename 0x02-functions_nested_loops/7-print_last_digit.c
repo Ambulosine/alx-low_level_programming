88f86e558f2488773cb7e78ae7d60a28125c4aa1
Ambulose <ambulose2019@gmail.com>
2022-10-18 16:42:45 +0100

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - print last digit of integer
+ * @n: integer argument
+ * Return: last digit of number
+ */
+
+int print_last_digit(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n % 10);
+}
