039328ae555aaee82895fa7efbe60e94363b24c2
Ambulose <ambulose2019@gmail.com>
2022-10-18 13:23:51 +0100

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet - Print Alphabets in lower case
+* Return:  Success Always
+*/
+
+void print_alphabet(void)
+{
+	int start = 'a';
+
+	while (start <= 'z')
+	{
+	_putchar(start);
+	start++;
+	}
+	putchar('\n');
+}
