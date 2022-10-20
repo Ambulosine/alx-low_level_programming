fabe5a03a1a0f3dc75b0068ef64b8cce429cd8d1
Ambulose <ambulose2019@gmail.com>
2022-10-20 14:08:44 +0100

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,11 @@
 
 /**
  * _isupper - A function that checks for uppercase character.
- * @r: An input character
+ * @c: An input character
  * Return: 1 if c is uppercase or 0 otherwise
  */
 
-int _isupper(int r)
+int _isupper(int c)
 {
-	return (r >= "A" && r <= "Z");
+	return (c >= "A" && c <= "Z");
 }
