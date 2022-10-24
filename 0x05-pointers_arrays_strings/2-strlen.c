a2a7d9d785bf575ae81fe420610149f229181baf
Ambulose <ambulose2019@gmail.com>
2022-10-24 15:50:29 +0100

    str length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen -> function to check string length
+ * @s: string to checked
+ * Return: length of the string
+*/
+
+int _strlen(char *s)
+{
+	int len;
+
+	for (; *s != '\0'; *s++)
+	{
+		len++
+	}
+	return (len);
+}
