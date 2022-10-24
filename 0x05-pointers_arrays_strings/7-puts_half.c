6691174376aa159a6270815312bdb4c983bbde51
Ambulose <ambulose2019@gmail.com>
2022-10-24 17:59:08 +0100

    fix

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half -> puts half function
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+	int a, b, i;
+
+	a = strlen(str);
+	if (a % 2 == 1)
+	{
+		y = (x / 2) + 1;
+	}
+	else
+	{
+		y = x / 2;
+	}
+	for (i = y; i < x; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
