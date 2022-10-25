493c39300fe6477c620a00dcfcccada2c2e08058
Ambali Olamilekan Ambali <109503950+Ambulosine@users.noreply.github.com>
2022-10-25 12:38:42 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,22 @@
 #include "main.h"
 
 /**
-* _puts -> prints a string followed by a newline to stdout
-* @str: string to be printed
-* Return: nothing
-*/
-void _puts(char *str)
+ * _strlen - returns the length of a given string
+ * @s: string for which length is to be calculated
+ * Return: length of s
+ */
+int _strlen(char *s)
 {
-	while (*str != 0)
+	char c;
+	int i;
+
+	c = *s;
+	i = 0;
+	while (c != 0)
 	{
-		_putchar(*str);
-		str++;
+		s++;
+		c = *s;
+		i++;
 	}
-	_putchar('\n');
+	return (i);
 }
