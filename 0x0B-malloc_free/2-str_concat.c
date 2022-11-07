6e50f2062953aa7d60f69c02521ceb1f01f18a1c
Ambulose <ambulose2019@gmail.com>
2022-11-07 15:17:17 +0100

    str cat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,51 +1,53 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stddef.h>
+#include <stdio.h>
 
-int len(char *s);
 /**
- * str_concat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * Return: pointer to the result
- */
+  * str_concat - Concatenates two strings of any size
+  * @s1: the first string to concatenate
+  * @s2: the second string to concatenate
+  *
+  * Return: the two strings concatenated
+  */
 char *str_concat(char *s1, char *s2)
 {
+	int i = 0, j = 0, k = 0, l = 0;
 	char *s;
-	int i, j, len_s;
 
-	len_s = 0;
-	if (s1 == NULL && s2 == NULL)
-		len_s++;
-	else if (s1 == NULL && s2 != NULL)
-		len_s = 1 + len(s2);
-	else if (s1 != NULL && s2 == NULL)
-		len_s = len(s1) + 1;
-	else
-		len_s = len(s1) + len(s2);
+	if (s1 == NULL)
+		s1 = "";
 
-	s = malloc(sizeof(char) * len_s);
-	if (s != NULL)
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[i])
+		i++;
+
+	while (s2[j])
+		j++;
+
+	l = i + j;
+	s = malloc((sizeof(char) * l) + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	j = 0;
+
+	while (k < l)
 	{
-		for (i = 0 ; i < len(s1); i++)
-			*(s + i) = *(s1 + i);
-		for (j = 0; j < len(s2); j++)
-			*(s + i + j) = *(s2 + j);
-		*(s + len_s) = '\0';
+		if (k <= i)
+			s[k] = s1[k];
+
+		if (k >= i)
+		{
+			s[k] = s2[j];
+			j++;
+		}
+
+		k++;
 	}
 
+	s[k] = '\0';
 	return (s);
-}
-
-/**
- * len - finds the length of a string
- * @s: the string
- * Return: length of s
- */
-int len(char *s)
-{
-	if (*s == 0 || s == NULL)
-		return (0);
-	else
-		return (len(s + 1) + 1);
 }
