69f55c25b7327f916091a8cb2ded4007dab4f459
Ambulose <ambulose2019@gmail.com>
2022-11-10 10:58:31 +0100

    mallo check

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#endif
